f5d2e94bd7e4146110cb6c3eda75aaba26ef0f6e
SEC-MASUM <masum.jess42@gmail.com>
2022-07-07 22:44:02 +0600

    feat: set routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl text-red-500">Clean Co</h1>
-      <button className="btn btn-primary">Hello</button>
-    </div>
+    <Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Navbar>
   );
 }
 
